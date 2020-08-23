3ad4dd9a0c3f4e85129c040f6c5adbba59cefe70
AaronDusy <arron@192-168-1-109.tpgi.com.au>
2020-08-24 06:53:00 +1000

    70 percent complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,10 +6,12 @@ public class LoadAssets : MonoBehaviour
 {
 
     public GameObject redObj;
+    [SerializeField] private GameObject blueObj;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(-2, 0, 0);
+        Instantiate(redObj,transform.position,Quaternion.identity);
     }
 
     // Update is called once per frame
